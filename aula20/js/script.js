7e09d4ee7b62923a226c33696319ac33be68f5a8
lealrenato <renatolealhiss@gmail.com>
2021-12-27 21:04:09 -0300

    master

---
+++ aula20/js/script.js
@@ -0,0 +1,17 @@
+function meuEscopo(s) {
+    const form = document.querySelector(".form");
+
+    function receberEventoForm(evento) {
+        evento.preventDefault();
+        const nome = form.querySelector(".nome");
+        const sobrenome = form.querySelector(".sobrenome");
+        const peso = form.querySelector(".peso");
+        const altura = form.querySelector(".altura");
+
+        console.log(nome.value, sobrenome.value, peso.value, altura.value);
+    }
+
+    form.addEventListener('submit', receberEventoForm);
+
+}
+meuEscopo();
---
+++ aula6/index.js
@@ -0,0 +1,8 @@
+let nome = 'Renato';
+console.log(
+    nome, 'Nasceu em 1983'
+)
+
+console.log(
+   'Em 2018', nome, 'Casou com Maria Laura'
+)
